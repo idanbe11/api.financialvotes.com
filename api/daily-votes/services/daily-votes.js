112cce65fc0f18bc28ab799a0947aa03ad054723
Ganindu Dhananja <ganindudananja@gmail.com>
2021-10-26 23:06:14 +0530

    Added new Daily Votes API

---
+++ api/daily-votes/services/daily-votes.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const _ = require('lodash');
+
+/**
+ * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-services)
+ * to customize this service
+ */
+
+module.exports = {
+  /**
+   * Promise to add/update the record
+   *
+   * @return {Promise}
+   */
+  async findOrCreate(coin_id, userId) {
+    let entry;
+    entry = await strapi.query('daily-votes').findOne({ coin_id });
+    if (!entry) {
+      entry = await strapi.query('daily-votes').create({ coin_id, votes: 0, updated: new Date() });
+    }
+    // strapi.log.debug('Found:', entry);
+    return entry;
+  },
+};
